3a0c37d7fa4cf82abbd0cf52498e07d36790cf69
jamejamekhan-cmd <jamejamekhan@gmail.com>
2026-08-03 12:11:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Crypto EMA Scanner Started")
+print("🚀 Crypto EMA Scanner Started")
+
+coins = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "BNBUSDT",
+    "SOLUSDT",
+    "XRPUSDT",
+    "DOGEUSDT",
+    "ADAUSDT",
+    "TRXUSDT",
+    "LINKUSDT",
+    "AVAXUSDT",
+    "SUIUSDT",
+    "TONUSDT",
+    "DOTUSDT",
+    "LTCUSDT",
+    "BCHUSDT",
+    "UNIUSDT",
+    "ATOMUSDT",
+    "APTUSDT",
+    "NEARUSDT",
+    "HBARUSDT"
+]
+
+for coin in coins:
+    print(f"Scanning {coin}...")
